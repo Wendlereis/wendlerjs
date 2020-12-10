e9a8152ceff53eb3ba3ac950b4787725fff274b4
Wendler Boaventura <wendlerboaventura@Wendler-Boaventura.local>
2020-12-10 11:32:52 -0300

    feat: add babel transformation

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 /** @jsx createElement */
-import { render, createElement } from './wendler.js'
+
+function createTextElement(value) {
+  return createElement('TEXT_ELEMENT', { nodeValue: value });
+}
+
+function createElement(type, config, ...children) {
+  const props = Object.assign({}, config);
+  const hasChildren = children.length > 0;
+  const rawChildren = hasChildren ? [].concat(...children) : [];
+  
+  props.children = rawChildren
+    .filter((child) => child !== null && child !== false)
+    .map((child) =>
+      child instanceof Object ? child : createTextElement(child)
+    );
+
+  return { type, props };
+}
+
+function render(element, node) {
+  const { type, props } = element;
+
+  const isTextElement = type === 'TEXT_ELEMENT' 
+
+  const dom = isTextElement 
+    ? document.createTextNode('')
+    : document.createElement(type);
+  
+  const isThisPropListener = (name) => name.startsWith('on');
+  
+  const isThisPropAttribute = (name) =>
+    !isThisPropListener(name) && name != 'children';
+  
+  Object.keys(props).map((item) => {
+    // add events
+    if (isThisPropListener(item)) {
+      const eventName = item.toLocaleLowerCase().substring(2);
+      dom.addEventListener(eventName, props[item]);
+    }
+    // add attributes
+    if (isThisPropAttribute(item)) {
+      dom[item] = props[item];
+    }
+  });
+
+  const childs = props.children || [];
+  
+  childs.forEach((childElement) => render(childElement, dom));
+  
+  node.appendChild(dom);
+}
 
 const jsxElement = (
   <div id="container">
@@ -7,4 +57,4 @@ const jsxElement = (
   </div>
 );
 
-render(jsxElement, document.getElementById("root"));
+render(jsxElement, document.getElementById('root'));
